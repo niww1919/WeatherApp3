11e8acc616c19ad4d05bce72a94d15c91c2a1960
niww <niww@ya.ru>
2020-01-04 15:13:31 +0300

    1-4

---
+++ app/src/main/java/com/example/weatherapp3/MainWeatherFragment.java
@@ -0,0 +1,39 @@
+package com.example.weatherapp3;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainWeatherFragment extends Fragment {
+
+    public MainWeatherFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_weather, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
