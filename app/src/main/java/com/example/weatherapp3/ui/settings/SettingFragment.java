9acd804903b2f51d6554d48c6b55b388a4e3c5ba
niww <niww@ya.ru>
2020-01-04 13:27:14 +0300

    1-4

--- app/src/main/java/com/example/weatherapp3/ui/settings/SettingFragment.java
+++ app/src/main/java/com/example/weatherapp3/ui/settings/SettingFragment.java
@@ -1,5 +1,7 @@
 package com.example.weatherapp3.ui.settings;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,6 +18,7 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.weatherapp3.R;
 
 public class SettingFragment extends Fragment {
+    public static final String LINK_TO_GITHUB = "https://github.com/niww1919/WeatherApp3";
 
     private SettingsViewModel settingsViewModel;
 
@@ -22,7 +26,7 @@ public class SettingFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         settingsViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +34,20 @@ public class SettingFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+        root.findViewById(R.id.buttonLinkToGitHub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri link = Uri.parse(LINK_TO_GITHUB);
+                Intent intent = new Intent(Intent.ACTION_VIEW, link);
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
+
+
 }
